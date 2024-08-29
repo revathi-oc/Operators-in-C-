bcea82525a2d12ab9ab42cc73ff7035322334c2c
revathi-oc <revathioc15@gmail.com>
2024-08-29 13:35:25 +0530

    Add files via upload

---
+++ operators.cpp
@@ -0,0 +1,66 @@
+//REVATHI OC
+//23070123105
+#include<iostream>
+using namespace std;
+/*int main()
+{
+    int a,b;
+    cout<<"Enter value of a: ";
+    cin>>a;
+    cout<<"Enter value of b: ";
+    cin>>b;
+     cout<<"a==b : "<<(a==b)<<endl;
+    cout<<"a!=b: "<<(a!=b)<<endl;
+    cout<<"a>b: "<<(a>b)<<endl;
+    cout<<"a<b: "<<(a<b)<<endl;
+    cout<<"a>=b: "<<(a>=b)<<endl;
+    cout<<"a<=b: "<<(a<=b)<<endl;
+    cout<<"a!=b: "<<(a!=b)<<endl;
+    return 0;
+    
+}
+
+
+//for getting output as true and false
+int main()
+{
+    int a,b;
+    cout<<"Enter value of a: ";
+    cin>>a;
+    cout<<"Enter value of b: ";
+    cin>>b;
+    cout<<boolalpha;
+    cout<<"a==b : "<<(a==b)<<endl;
+    cout<<"a!=b: "<<(a!=b)<<endl;
+    cout<<"a>b: "<<(a>b)<<endl;
+    cout<<"a<b: "<<(a<b)<<endl;
+    cout<<"a>=b: "<<(a>=b)<<endl;
+    cout<<"a<=b: "<<(a<=b)<<endl;
+    cout<<"a!=b: "<<(a!=b)<<endl;
+    return 0;
+
+}
+
+//logical operators
+int main()
+{
+    bool A=true,b=false;
+    cout<<boolalpha;
+    cout<<"a&&b: "<<(A&&b)<<endl;
+    cout<<"a||b: "<<(A||b)<<endl;
+    return 0;
+
+}*/
+
+// logical operators
+int main()
+{
+    int a=2,b=3;
+    cout<<boolalpha;
+    cout<<"a&b: "<<(a&b)<<endl;
+    cout<<"a|b: "<<(a|b)<<endl;
+    cout<<"a^b: "<<(a^b)<<endl;
+    cout<<"a<<b: "<<(a<<b)<<endl;
+    cout<<"a>>b: "<<(a>>b)<<endl;
+    return 0;
+}
---
+++ operators_arithematic.cpp
@@ -0,0 +1,19 @@
+//REVATHI OC
+//23070123105
+#include<iostream>
+using namespace std;
+int main()
+{
+    int A,B;
+    cout<<"Enter 1st number: ";
+    cin>>A;
+    cout<<"Enter 2nd number: ";
+    cin>>B;
+    cout<<"Addition of "<<A<<" and "<<B <<" is: "<<(A+B)<<endl;;
+    cout<<"Subtraction of "<<A<<" and "<<B <<" is: "<<(A-B)<<endl;;
+    cout<<"Multiplication of "<<A<<" and "<<B <<" is: "<<(A*B)<<endl;;
+    cout<<"Division of "<<A<<" and "<<B <<" is: "<<(A/B)<<endl;
+    cout<<"Modulus of "<<A<<" and "<<B <<" is: "<<(A%B)<<endl;
+    return 0;
+
+}
